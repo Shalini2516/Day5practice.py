54f48dffc8c84e6d532f4132dbd4eac3e54c9ae8
Shalini2516 <Saamkm@3>
2023-07-31 13:22:33 -0400

    day 4

---
+++ Day4practice.py
@@ -0,0 +1,19 @@
+a1=[10,20,50,'me',12.6,'Welcome']
+print(a1[3])
+print(a1[1])
+print(a1[0:4])
+print(a1[2:7])
+a1.append(23)#adding the data to list
+print (a1)
+a1.insert(4,'hi')#inserting data at particular index
+print (a1)
+a1[4]='hye'#replacing the value directly by giving the index position
+print (a1)
+a1.remove(12.6)
+print (a1)
+a1.pop(3)
+print (a1)
+a1.pop()
+print (a1)
+a1.pop()
+print(a1)
